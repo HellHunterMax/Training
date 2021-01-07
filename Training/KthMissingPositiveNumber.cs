48e63249cddc41ac552a7368a51c788d9055cd3b
Max <hellhunter15@hotmail.com>
2021-01-07 06:00:22 +0100

    Add project files.

---
+++ Training/KthMissingPositiveNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training
+{
+    public static class KthMissingPositiveNumber
+    {
+        public static int FindKthPositive(int[] arr, int k)
+        {
+            int finalNumber = 0;
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int numberArrINeedsToBe = i + count + 1;
+                if (arr[i] != numberArrINeedsToBe)
+                {
+                    int addToCount = arr[i] - numberArrINeedsToBe;
+
+                    if (count + addToCount >= k)
+                    {
+                        addToCount = k - count;
+                        finalNumber += addToCount;
+                        return finalNumber;
+                    }
+                    else
+                    {
+                        count += addToCount;
+                    }
+                }
+                finalNumber = arr[i];
+            }
+
+            finalNumber += (k - count);
+
+            return finalNumber;
+        }
+    }
+}
---
+++ Training/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
