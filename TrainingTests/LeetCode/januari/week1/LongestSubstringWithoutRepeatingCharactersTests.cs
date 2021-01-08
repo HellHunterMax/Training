a97803ea8d31187385bd3a7aecd8487d35fe8f81
Max <hellhunter15@hotmail.com>
2021-01-08 21:24:15 +0100

    changed folder structure;

---
+++ TrainingTests/LeetCode/januari/week1/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,20 @@
+﻿using Training;
+using Xunit;
+
+namespace TrainingTests
+{
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [InlineData(3, "abcabcbb")]
+        [InlineData(1, "bbbbb")]
+        [InlineData(0, "")]
+        [InlineData(3, "pwwkew")]
+        [Theory]
+        public void TheoryTest(int expected, string s)
+        {
+            int actual = LongestSubstringWithoutRepeatingCharacters.Get(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
