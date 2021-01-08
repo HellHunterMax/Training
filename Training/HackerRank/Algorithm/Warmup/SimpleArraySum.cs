5a19c5c82aebc9004a49b9d0dbc5ed9314c5d79d
Max <hellhunter15@hotmail.com>
2021-01-08 21:38:54 +0100

    Added HackerRank; started first Test SimpleArraytSum; added test for SimpleArraySum;

---
+++ Training/HackerRank/Algorithm/Warmup/SimpleArraySum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.HackerRank.Algorithm.Warmup
+{
+    public static class SimpleArraySum
+    {
+    }
+}
---
+++ TrainingTests/HackerRank/Algorithm/Warmup/SimpleArraySumTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingTests.HackerRank.Algorithm.Warmup
+{
+    public class SimpleArraySumTests
+    {
+
+    }
+}
