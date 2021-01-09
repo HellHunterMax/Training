ad90a81a16bd2465fe86a384c6370da723e4be39
Max <hellhunter15@hotmail.com>
2021-01-09 21:58:30 +0100

    Added WordLadder class and unit tests;

---
+++ Training/LeetCode/januari/week2/WordLadder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week2
+{
+    public static class WordLadder
+    {
+        public static int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        {
+            int LadderLength = 0;
+            return LadderLength;
+        }
+    }
+}
---
+++ TrainingTests/LeetCode/januari/week2/WordLadderTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Training.LeetCode.januari.week2;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.week2
+{
+    public class WordLadderTests
+    {
+        [InlineData(5, "hot", "cog", new string[] {"hot", "dot", "dog", "lot", "log", "cog"})]
+        [InlineData(0, "hot", "cog", new string[] { "hot", "dot", "dog", "lot", "log" })]
+        [InlineData(0, "hot", "cog", new string[] { "cog", "dot", "dog", "lot", "log", "hot" })]
+        [InlineData(5, "hot", "cog", new string[] { "hot", "hot", "dog", "lot", "log", "cog" })]
+        [Theory]
+        public void WordLadder_Given_ListOfWordsAndStartAndStop_Returns_Length(int expected, string beginWord, string endWord, IList<string> wordList)
+        {
+            int actual = WordLadder.LadderLength(beginWord, endWord, wordList);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
