612affc4946558cc76d820e9bee193779f6b3831
Max <hellhunter15@hotmail.com>
2021-01-10 21:46:08 +0100

    CreateSortedArrayThroughInstructions tests and class;

---
+++ Training/LeetCode/januari/week2/CreateSortedArrayThroughInstructions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week2
+{
+    public static class CreateSortedArrayThroughInstructions
+    {
+        public static int CreateSortedArray(int[] instructions)
+        {
+            int sum = 0;
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                int number = instructions[i];
+                list.Add(number);
+                list.Sort();
+                int firstIndex = list.IndexOf(number);
+                int lastIndex = list.LastIndexOf(number);
+
+                int distance = list.Count - 1 - lastIndex;
+
+                sum += firstIndex < distance ? firstIndex : distance;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ TrainingTests/LeetCode/januari/week2/CreateSortedArrayThroughInstructionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Training.LeetCode.januari.week2;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.week2
+{
+    public class CreateSortedArrayThroughInstructionsTests
+    {
+        [InlineData(1, new int[] { 1, 5, 6, 2 })]
+        [InlineData(3, new int[] { 1, 2, 3, 6, 5, 4 })]
+        [InlineData(4, new int[] { 1, 3, 3, 3, 2, 4, 2, 1, 2 })]
+        [InlineData(0, new int[] { 1, 2 })]
+        [InlineData(0, new int[] { 1, 1, 1, 1, 1, 1, 1 })]
+        [Theory]
+        public void CreateSortedArrayThroughInstructions_Given_Array_Returns_CostOfSorting(int expected, int[] arr)
+        {
+            int actual = CreateSortedArrayThroughInstructions.CreateSortedArray(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
