81b43b94fc0a19f1e2b715f302ce60c03d06ddd3
Max <hellhunter15@hotmail.com>
2021-01-12 00:59:03 +0100

    Created AVeryBigSum Class; removed WordLadder;

---
+++ Training/HackerRank/Algorithm/Warmup/AVeryBigSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Training.HackerRank.Algorithm.Warmup
+{
+    public static class AVeryBigSum
+    {
+        public static long aVeryBigSum(long[] ar)
+        {
+            long sum = ar.Sum();
+
+            return sum;
+
+        }
+    }
+}
