3e6a76f5931ef40cc99a54a2ab419d9902e42f14
Max <hellhunter15@hotmail.com>
2021-01-13 03:44:41 +0100

    2nd go at AddTwoLinkedListNumbers; Finished;

--- Training/LeetCode/januari/week2/AddTwoLinkedListNumbers.cs
+++ Training/LeetCode/januari/week2/AddTwoLinkedListNumbers.cs
@@ -19,7 +19,13 @@ namespace Training.LeetCode.januari.week2
     }
     public class AddTwoLinkedListNumbers
     {
+
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return AddTwoNumbersSecond(l1, l2);
+        }
+
+        public ListNode AddTwoNumbersFirst(ListNode l1, ListNode l2)
         {
             string number1 = string.Empty;
             string number2 = string.Empty;
@@ -52,11 +58,86 @@ namespace Training.LeetCode.januari.week2
 
             return listOfNodes[0];
         }
-        public static string Reverse(string s)
+        public ListNode AddTwoNumbersSecond(ListNode l1, ListNode l2)
+        {
+            string sum = String.Empty;
+
+            bool remain = false;
+            while ( l1 != null && l2 != null)
+            {
+                int num = l1.val + l2.val;
+                if (remain)
+                {
+                    num += 1;
+                    remain = false;
+                }
+                if (num >9)
+                {
+                    num -= 10;
+                    remain = true;
+                }
+                sum += num;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            while (l1 != null)
+            {
+                int num = l1.val;
+                if (remain)
+                {
+                    num += 1;
+                    remain = false;
+                }
+                if (num > 9)
+                {
+                    num -= 10;
+                    remain = true;
+                }
+                sum += num;
+                l1 = l1.next;
+            }
+            while (l2 != null)
+            {
+                int num = l2.val;
+                if (remain)
+                {
+                    num += 1;
+                    remain = false;
+                }
+                if (num > 9)
+                {
+                    num -= 10;
+                    remain = true;
+                }
+                sum += num;
+                l2 = l2.next;
+            }
+            if (remain)
+            {
+                sum += 1;
+            }
+            List<ListNode> listOfNodes = new List<ListNode>();
+
+            for (int i = 0; i < sum.Length; i++)
+            {
+                listOfNodes.Add(new ListNode(int.Parse(sum[i].ToString())));
+
+            }
+            for (int i = 0; i < listOfNodes.Count - 1; i++)
+            {
+                listOfNodes[i].next = listOfNodes[i + 1];
+            }
+
+            return listOfNodes[0];
+        }
+
+            public static string Reverse(string s)
         {
             char[] charArray = s.ToCharArray();
             Array.Reverse(charArray);
             return new string(charArray);
         }
+
+
     }
 }
