2111ab8707a83d6dca01b57bdcef5792fea8cf13
Max <hellhunter15@hotmail.com>
2021-01-14 05:06:52 +0100

    finished;

--- Training/LeetCode/januari/week2/BoatsToSavePeople.cs
+++ Training/LeetCode/januari/week2/BoatsToSavePeople.cs
@@ -1,15 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.LeetCode.januari.week2
 {
     public static class BoatsToSavePeople
     {
+
         public static int NumRescueBoats(int[] people, int limit)
         {
+            return NumRescueBoatsOwn(people, limit);
+        }
+        public static int NumRescueBoatsOwn(int[] people, int limit)
+        {
             int numberOfBoats = 0;
-            throw new NotImplementedException();
+
+            List<int> peopleList = people.ToList();
+
+            peopleList.Sort();
+
+            while (peopleList.Count > 1)
+            {
+                int last = peopleList.Count - 1;
+                numberOfBoats++;
+                if (peopleList[0] +  peopleList[last] <= limit)
+                {
+                    peopleList.RemoveAt(0);
+                    peopleList.RemoveAt(last -1);
+                }
+                else
+                {
+                    peopleList.RemoveAt(last);
+                }
+                
+            }
+            if (peopleList.Count == 1)
+            {
+                numberOfBoats++;
+            }
+            return numberOfBoats;
+        }
+
+        private static int NumRescueBoatsStolen(int[] people, int limit)
+        {
+            int numberOfBoats = 0;
+
+            List<int> peopleList = people.ToList();
+
+            peopleList.Sort();
+
+            var lightestPersonIndex = 0;
+            var heaviestPersonIndex = peopleList.Count - 1;
+            while (lightestPersonIndex <= heaviestPersonIndex)
+            {
+                numberOfBoats++;
+                if (peopleList[lightestPersonIndex] + peopleList[heaviestPersonIndex] <= limit)
+                {
+                    lightestPersonIndex++;
+                }
+                heaviestPersonIndex--;
+            }
             return numberOfBoats;
         }
     }
