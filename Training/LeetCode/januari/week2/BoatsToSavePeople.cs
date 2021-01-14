83a725bd058c63c49976917bef0e0b66aabe3b2d
Max <hellhunter15@hotmail.com>
2021-01-14 02:55:38 +0100

    Created Tests for BoatsToSavePeople; created Placeholder Class for BoatsToSavePeople;

---
+++ Training/LeetCode/januari/week2/BoatsToSavePeople.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week2
+{
+    public static class BoatsToSavePeople
+    {
+        public static int NumRescueBoats(int[] people, int limit)
+        {
+            int numberOfBoats = 0;
+            throw new NotImplementedException();
+            return numberOfBoats;
+        }
+    }
+}
--- TrainingTests/LeetCode/januari/week2/BoatsToSavePeopleTests.cs
+++ TrainingTests/LeetCode/januari/week2/BoatsToSavePeopleTests.cs
@@ -1,13 +1,20 @@
-﻿using Xunit;
+﻿using Training.LeetCode.januari.week2;
+using Xunit;
 
 namespace TrainingTests.LeetCode.januari.week2
 {
     public class BoatsToSavePeopleTests
     {
         [Theory]
+        [InlineData(1, new int[] { 1 }, 1)]
+        [InlineData(1, new int[] { 1,  2}, 3)]
+        [InlineData(3, new int[] { 3, 2, 2, 1 }, 3)]
+        [InlineData(4, new int[] { 3, 5, 3, 4 }, 5)]
         public void BoatsToSavePeople_Given_PeopleAndLimit_Returns_NumberOfBoats(int expected, int[] people, int limit)
         {
-            Assert.Equal(expected, actual)
+            int actual = BoatsToSavePeople.NumRescueBoats(people, limit);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
