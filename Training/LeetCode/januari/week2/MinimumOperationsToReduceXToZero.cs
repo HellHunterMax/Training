87cc1f03c38e6735d7d4d026339cfd991e02b83c
Max <hellhunter15@hotmail.com>
2021-01-15 00:11:57 +0100

    Created class and Tests for MinimumOperationsToReduceXToZero;

---
+++ Training/LeetCode/januari/week2/MinimumOperationsToReduceXToZero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week2
+{
+    public static class MinimumOperationsToReduceXToZero
+    {
+        public static int MinOperations(int[] nums, int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrainingTests/LeetCode/januari/week2/MinimumOperationsToReduceXToZeroTests.cs
@@ -0,0 +1,21 @@
+﻿using Training.LeetCode.januari.week2;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.week2
+{
+    public class MinimumOperationsToReduceXToZeroTests
+    {
+
+        [Theory]
+        [InlineData(2, new int[] { 1, 1, 4, 2, 3 }, 5)]
+        [InlineData(-1, new int[] { 1 }, 5)]
+        [InlineData(-1, new int[] { 2, 1, 4, 2, 3 }, 1)]
+        [InlineData(5, new int[] { 3, 2, 20, 1, 1, 3 }, 10)]
+        public void MinOperations_Given_ArrayAndInt_Returns_MinimumToGetToZero(int expected, int[] nums, int x)
+        {
+            int actual = MinimumOperationsToReduceXToZero.MinOperations(nums, x);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
