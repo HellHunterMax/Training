c39edccfb5cc1291b4141ed917cf5dc5b092a9fd
Max <hellhunter15@hotmail.com>
2021-01-15 05:36:27 +0100

    gave up for now on this one; come back later;

--- Training/LeetCode/januari/week2/MinimumOperationsToReduceXToZero.cs
+++ Training/LeetCode/januari/week2/MinimumOperationsToReduceXToZero.cs
@@ -1,14 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Training.LeetCode.januari.week2
 {
+    //Gave up on this one very hard for me to grasp and late... will watch more video's on the subject
+    //TODO watch more on this subject
     public static class MinimumOperationsToReduceXToZero
     {
+        /*
         public static int MinOperations(int[] nums, int x)
         {
-            throw new NotImplementedException();
+            return FirstGo(nums, x);
         }
+
+        private static int FirstGo(int[] nums, int x)
+        {
+            List<int> leftToRight = new List<int>();
+            List<int> rightToLeft = new List<int>();
+            int left = 0;
+            int right = nums.Length - 1;
+            int number = 0;
+            while (left <= right && number + nums[left] <= x)
+            {
+                number += nums[left];
+                leftToRight.Add(number);
+                left++;
+            }
+            number = 0;
+            while (right > 0 && number + nums[right] <= x)
+            {
+                number += nums[right];
+                rightToLeft.Add(number);
+                right--;
+            }
+            left = leftToRight.Count;
+            right = rightToLeft.Count;
+            int total = 0;
+            int biggestOneSideLeft = leftToRight.IndexOf(x);
+            int biggestOneSideRight = rightToLeft.IndexOf(x);
+            /*
+            x = 5[1, 1, 4, 2, 3];
+
+            left to right = [1, 2]; left = 2 biggest -1
+            right to left = [3, 5]; right = 2 biggest = 1
+            
+            11 - 5 = 6
+            sum -x = rest
+            rest - x = sum
+            if (left == 0 && right == 0)
+            {
+                //Both Sides start with higher number then x
+                return -1;
+            }
+            if (leftToRight[left - 1] + rightToLeft[right - 1] < x)
+            {
+                //sum of both is too low
+                return -1;
+            }
+
+            if (biggestOneSideLeft < 0)
+            {
+                //int number to check from is right
+            }
+            else
+            {
+                //int number to check from is left
+            }
+
+            for (int i = 0; i < left && i < right; i++)
+            {
+
+            }
+            return -1;
+        }
+
+        private static int AfteryYoutubeGo(int[] nums, int x)
+        {
+
+            //make a dictionary int int
+            // link number with sum start 
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            List<int> listNums = new List<int>();
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                map.Add(sum, i);
+                listNums.Add(sum);
+            }
+
+            int rest = sum - x;
+            int longest = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (listNums.GetRange(0, i).Contains(rest))
+                {
+                    longest = i + 1;
+                }
+            }
+        }
+        */
     }
+
 }
--- TrainingTests/LeetCode/januari/week2/MinimumOperationsToReduceXToZeroTests.cs
+++ TrainingTests/LeetCode/januari/week2/MinimumOperationsToReduceXToZeroTests.cs
@@ -11,6 +11,8 @@ namespace TrainingTests.LeetCode.januari.week2
         [InlineData(-1, new int[] { 1 }, 5)]
         [InlineData(-1, new int[] { 2, 1, 4, 2, 3 }, 1)]
         [InlineData(5, new int[] { 3, 2, 20, 1, 1, 3 }, 10)]
+        [InlineData(3, new int[] { 1, 40, 1, 1, 1, 1, 1, 1, 1, 1, 2, 33,1, 1, 1, 1, 1, 1, 1, 1, 9  }, 50)]
+
         public void MinOperations_Given_ArrayAndInt_Returns_MinimumToGetToZero(int expected, int[] nums, int x)
         {
             int actual = MinimumOperationsToReduceXToZero.MinOperations(nums, x);
