bea40a220dc61130074fbf7a0f7bbfad06f1b617
Max <hellhunter15@hotmail.com>
2021-01-19 00:45:23 +0100

    Created MaxNumberOfKSumPairs Tests; Created MaxNumberOfKSumPairs Class;

---
+++ Training/LeetCode/januari/week3/MaxNumberOfKSumPairs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Training.LeetCode.januari.week3
+{
+    public static class MaxNumberOfKSumPairs
+    {
+        public static  int MaxOperations(int[] nums, int k)
+        {
+
+            return FirstGo(nums, k);
+
+        }
+
+        private static int FirstGo(int[] nums, int k)
+        {
+            List<int> list = nums.ToList();
+
+            int count = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; k < list.Count; k++)
+                {
+                    if (list[i] + list[j] == k)
+                    {
+                        list.RemoveAt(j);
+                        list.RemoveAt(i);
+                        i--;
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ TrainingTests/LeetCode/januari/Week3/MaxNumberOfKSumPairsTests.cs
@@ -0,0 +1,18 @@
+﻿using Training.LeetCode.januari.week3;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.Week3
+{
+    public class MaxNumberOfKSumPairsTests
+    {
+
+        [Theory]
+        [InlineData(2, new int[] { 1, 2, 3, 4 }, 5)]
+        public void MaxNumberOfKSumPairs_Given_ArrayAndNumber_Returns_NumberOfOpperationsPossible(int expected, int[] nums, int k)
+        {
+            int actual = MaxNumberOfKSumPairs.MaxOperations(nums, k);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
