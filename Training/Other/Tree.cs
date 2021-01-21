33915b6ea6733696470c7f105def782afc1c5e87
Max <hellhunter15@hotmail.com>
2021-01-22 00:10:29 +0100

    Created Tree class (piramid Challenge); Created TreeTests;

---
+++ Training/Other/Tree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.Other
+{
+    public class Tree
+    {
+        public string[] XmasTree;
+
+        public Tree(int hight)
+        {
+
+        }
+    }
+}
