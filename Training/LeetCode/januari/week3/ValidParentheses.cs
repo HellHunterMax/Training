2999fe6cc9dcb2633ccb749084ce3f6bab0a5bb7
Max <hellhunter15@hotmail.com>
2021-01-21 04:20:49 +0100

    created ValidParentheses class;

---
+++ Training/LeetCode/januari/week3/ValidParentheses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week3
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            bool answer = false;
+
+            return false;
+        }
+    }
+}
