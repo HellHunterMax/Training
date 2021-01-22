5bfa8216870119c7a050334211a0b24658df8d3d
Max <hellhunter15@hotmail.com>
2021-01-22 01:35:46 +0100

    Finished FirstTry;

--- Training/LeetCode/januari/week3/FindTheMostCompetitiveSubsequence.cs
+++ Training/LeetCode/januari/week3/FindTheMostCompetitiveSubsequence.cs
@@ -8,7 +8,29 @@ namespace Training.LeetCode.januari.week3
     {
         public static int[] MostCompetitive(int[] nums, int k)
         {
-            throw new NotImplementedException();
+            return FirstTry(nums, k);
+        }
+
+        private static int[] FirstTry(int[] nums, int k)
+        {
+            int[] winner = new int[k];
+            int last = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                winner[i] = int.MaxValue;
+
+                for (int j = last; j <= nums.Length - (k - i); j++)
+                {
+
+                    if (nums[j] < winner[i])
+                    {
+                        last = j + 1;
+                        winner[i] = nums[j];
+                    }
+                }
+            }
+            return winner;
         }
     }
 }
