fbe226c6ea2a860ed7d8ae3a7e91aadc50d5e89e
Max <hellhunter15@hotmail.com>
2021-01-23 01:12:03 +0100

    finished DetermineIfTwoStringsAreClose challenge;

---
+++ TrainingTests/LeetCode/januari/week4/DetermineIfTwoStringsAreCloseTests.cs
@@ -0,0 +1,21 @@
+﻿using Training.LeetCode.januari.week4;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.week4
+{
+    public class DetermineIfTwoStringsAreCloseTests
+    {
+        [Theory]
+        [InlineData(true, "","")]
+        [InlineData(false, "a", "cd")]
+        [InlineData(true, "cabbba", "abbccc")]
+        [InlineData(false, "abbzzca", "babzzcz")]
+        [InlineData(false, "abbzzcag", "babzzczd")]
+        public void DetermineIfTwoStringsAreClose_Given_TwoStrings_Returns_CloseOrFalse(bool expected, string word1, string word2)
+        {
+            var actual = DetermineIfTwoStringsAreClose.CloseStrings(word1, word2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
