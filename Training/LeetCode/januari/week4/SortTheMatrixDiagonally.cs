6c388f4f9315158cf6e13284f8274b9ac625f886
Max <hellhunter15@hotmail.com>
2021-01-24 00:21:12 +0100

    created placeholder SortTheMatrixDiagonally;

---
+++ Training/LeetCode/januari/week4/SortTheMatrixDiagonally.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week4
+{
+    public class SortTheMatrixDiagonally
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
