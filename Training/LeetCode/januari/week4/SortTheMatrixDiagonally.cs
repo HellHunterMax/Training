4dbf997dd50bf48d462afcb74fb6e99cc5b87538
Max <hellhunter15@hotmail.com>
2021-01-24 02:18:57 +0100

    done; There are improvements i could make;

--- Training/LeetCode/januari/week4/SortTheMatrixDiagonally.cs
+++ Training/LeetCode/januari/week4/SortTheMatrixDiagonally.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Training.LeetCode.januari.week4
@@ -8,7 +9,54 @@ namespace Training.LeetCode.januari.week4
     {
         public static int[][] DiagonalSort(int[][] mat)
         {
-            throw new NotImplementedException();
+            return FirstTry(mat);
+        }
+
+        public static int[][] FirstTry(int[][] mat)
+        {
+            int height = mat.Length;
+            int width = mat[0].Length;
+
+            int[][] final = mat;
+
+            List<List<int>> diagonnal = new List<List<int>>();
+
+            for (int i = 0; i < width + height -1; i++)
+            {
+                diagonnal.Add(new List<int>());
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int index = j - i;
+                    if (index < 0)
+                    {
+                        index = Math.Abs(index) + width -1;
+                    }
+                    diagonnal[index].Add(mat[i][j]);
+                }
+            }
+            for (int i = 0; i < width + height - 1; i++)
+            {
+                diagonnal[i].Sort();
+            }
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int index = j - i;
+                    if (index < 0)
+                    {
+                        index = Math.Abs(index) + width - 1;
+                    }
+                    mat[i][j] = diagonnal[index].First();
+                    diagonnal[index].Remove(diagonnal[index].First());
+                }
+            }
+
+            return final;
         }
     }
 }
--- TrainingTests/LeetCode/januari/week4/SortTheMatrixDiagonallyTests.cs
+++ TrainingTests/LeetCode/januari/week4/SortTheMatrixDiagonallyTests.cs
@@ -8,10 +8,10 @@ namespace TrainingTests.LeetCode.januari.week4
         [Fact]
         public void SortTheMatrixDiagonally_Given_Matrix_Returns_Sorted_Matrix()
         {
-            int[][] expected = new int[][] { new int[] { 3, 3, 1, 1 }, new int[] { 2, 2, 1, 2 }, new int[] { 1, 1, 1, 2 } };
-            int[][] mat = new int[][] { new int[] { 1, 1, 1, 1 }, new int[] { 1, 2, 2, 2 }, new int[] { 1, 2, 3, 3 } };
+            int[][] mat = new int[][] { new int[] { 3, 3, 1, 1 }, new int[] { 2, 2, 1, 2 }, new int[] { 1, 1, 1, 2 } };
+            int[][] expected = new int[][] { new int[] { 1, 1, 1, 1 }, new int[] { 1, 2, 2, 2 }, new int[] { 1, 2, 3, 3 } };
 
-            var actual = SortTheMatrixDiagonally.DiagonalSort(mat);
+            int[][] actual = SortTheMatrixDiagonally.DiagonalSort(mat);
 
             Assert.Equal(expected, actual);
         }
