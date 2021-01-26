c415f88b2c00ac709f992d52884ddd197873eccd
Max <hellhunter15@hotmail.com>
2021-01-26 11:56:44 +0100

    created CheckIfAll1sAreAtLeastLengthKPlacesAway tests; and placeholder class;

---
+++ Training/LeetCode/januari/week4/CheckIfAll1sAreAtLeastLengthKPlacesAway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week4
+{
+    public static class CheckIfAll1sAreAtLeastLengthKPlacesAway
+    {
+        public static bool KLengthApart(int[] nums, int k)
+        {
+
+        }
+    }
+}
