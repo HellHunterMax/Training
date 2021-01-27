8aa48959950e2546b6b5cc6366982abcc8c92d27
Max <hellhunter15@hotmail.com>
2021-01-28 00:38:57 +0100

    created Class for ConcatenationOfConsecutiveBinaryNumbers Chalenge;

---
+++ Training/LeetCode/januari/week4/ConcatenationOfConsecutiveBinaryNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week4
+{
+    public static class ConcatenationOfConsecutiveBinaryNumbers
+    {
+        public static int ConcatenatedBinary(int n)
+        {
+            return FirstTry(n);
+        }
+
+        public static int FirstTry(int n)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
