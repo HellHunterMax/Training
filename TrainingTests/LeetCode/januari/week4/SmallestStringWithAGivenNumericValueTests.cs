cf9e9559776f9df46efc18c6458dedc5651cb85b
Max <hellhunter15@hotmail.com>
2021-01-29 01:46:35 +0100

    Created Tests;

---
+++ TrainingTests/LeetCode/januari/week4/SmallestStringWithAGivenNumericValueTests.cs
@@ -0,0 +1,18 @@
+﻿using Training.LeetCode.januari.week4;
+using Xunit;
+
+namespace TrainingTests.LeetCode.januari.week4
+{
+    public class SmallestStringWithAGivenNumericValueTests
+    {
+        [Theory]
+        [InlineData(3, 27, "aay")]
+        [InlineData(5, 73, "aaszz")]
+        public void SmallestStringWithAGivenNumericValue_Given_TwoNumbers_Returns_CorrectString(int nLength, int kValue, string expected)
+        {
+            string actual = SmallestStringWithAGivenNumericValue.GetSmallestString(nLength, kValue);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
