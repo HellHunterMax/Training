d079a92f017fd6da18b7cc3872ecb0eda22ceeba
Max <hellhunter15@hotmail.com>
2021-01-29 01:42:01 +0100

    created placeholder SmallestStringWithAGivenNumericValue class;

---
+++ Training/LeetCode/januari/week4/SmallestStringWithAGivenNumericValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.januari.week4
+{
+    public static class SmallestStringWithAGivenNumericValue
+    {
+        public static string GetSmallestString(int n, int k)
+        {
+            return FirstTry(n, k);
+        }
+
+        public static string FirstTry(int nLength, int kValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
