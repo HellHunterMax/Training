e31eefd178be0e13c0051c2ef1978491da1b6dd4
Max <hellhunter15@hotmail.com>
2021-02-04 02:05:29 +0100

    added first go at LinkedListCycle;

--- Training/LeetCode/februari/week1/LinkedListCycle.cs
+++ Training/LeetCode/februari/week1/LinkedListCycle.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace Training.LeetCode.februari.week1
 {
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int x)
+        {
+            val = x;
+            next = null;
+                 }
+  }
     public static class LinkedListCycle
     {
+        public static bool HasCycle(ListNode head)
+        {
+            ListNode node = head;
+            List<ListNode> listOfNodes = new List<ListNode>();
+            while(node.next != null)
+        {
+                listOfNodes.Add(node);
+            if (listOfNodes.Contains(node.next))
+                {
+                    return true;
+                }
+                node = node.next;
+            }
+            return false;
+        }
     }
 }
