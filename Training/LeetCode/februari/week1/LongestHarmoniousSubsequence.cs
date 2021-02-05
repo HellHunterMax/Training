990b64545627e62c2f1f86cee7ece4e520b340aa
Max <hellhunter15@hotmail.com>
2021-02-05 05:24:51 +0100

    finished after 1 speedup.

--- Training/LeetCode/februari/week1/LongestHarmoniousSubsequence.cs
+++ Training/LeetCode/februari/week1/LongestHarmoniousSubsequence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Training.LeetCode.februari.week1
@@ -12,8 +13,26 @@ namespace Training.LeetCode.februari.week1
         }
         private static int FirstGo(int[] nums)
         {
-            throw new NotImplementedException();
+            var diff = (from num in nums
+                            group num by num
+                            into g orderby g.Key ascending
+                            select new { Count = g.Count(), Value = g.Key }).ToList();
+
+            List<int> possibles = new List<int>();
+            for (int i = 1; i < diff.Count(); i++)
+            {
+                if(diff[i].Value - diff[i -1].Value == 1)
+                {
+                    possibles.Add(diff[i].Count + diff[i - 1].Count);
+                }
+            }
+
+            if (possibles.Count == 0)
+            {
+                return 0;
+            }
+
+            return possibles.Max();
         }
     }
-}
 }
