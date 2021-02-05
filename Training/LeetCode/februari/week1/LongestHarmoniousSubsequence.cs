3108efecb82f76e3861735f0fabbd498862d51e7
Max <hellhunter15@hotmail.com>
2021-02-05 04:09:07 +0100

    created placeholder LongestHarmoniousSubsequence class;

---
+++ Training/LeetCode/februari/week1/LongestHarmoniousSubsequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week1
+{
+    public static class LongestHarmoniousSubsequence
+    {
+        public static int FindLHS(int[] nums)
+        {
+            FirstGo(nums);
+        }
+        private static int FirstGo(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
