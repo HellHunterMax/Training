46c767d4465760c0bdb55743d34614268cee41ab
Max <hellhunter15@hotmail.com>
2021-02-06 01:38:40 +0100

    created Tests;

---
+++ TrainingTests/LeetCode/februari/week1/SimplifyPathTests.cs
@@ -0,0 +1,20 @@
+﻿using Training.LeetCode.februari.week1;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week1
+{
+    public class SimplifyPathTests
+    {
+        [InlineData("/home", "/home/")]
+        [InlineData("/", "/../")]
+        [InlineData("/home/foo", "/home//foo/")]
+        [InlineData("/c", "/a/./b/../../c/")]
+        [Theory]
+        public void SimplifyPath_Given_Path_Simplifies(string expected, string input)
+        {
+            var actual = SimplifyPath.Simplify(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
