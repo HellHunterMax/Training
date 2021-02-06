e9ce56331e2ddfa5232ebeed6f0f5f35959d3323
Max <hellhunter15@hotmail.com>
2021-02-06 03:24:49 +0100

    some refactoring;

--- Training/LeetCode/februari/week1/SimplifyPath.cs
+++ Training/LeetCode/februari/week1/SimplifyPath.cs
@@ -6,35 +6,33 @@ namespace Training.LeetCode.februari.week1
 {
     public static class SimplifyPath
     {
+        private const string _seperator = "/";
+        private const string _back = "..";
+        private const string _dot = ".";
+
         public static string Simplify(string path)
         {
             return FirstTry(path);
         }
-
-        /*
-         * 
-        ///// any number == /
-        . == current lvl so can be deleted
-        .. == one lvl back if possible
-        .. > == path
-        path starts with /
-        no trailing /
-
-        */
-
         private static string FirstTry(string path)
         {
-            const string seperator = "/";
-            const string back = "..";
-            const string dot = ".";
+            List<string> commandList = GetCommandList(path);
+            
+            string simple = _seperator;
+            simple += String.Join(_seperator, commandList);
+            return simple;
+        }
 
+        private static List<string> GetCommandList(string path)
+        {
             string command = string.Empty;
             string lastLetter = string.Empty;
             List<string> commandList = new List<string>();
+
             for (int i = 0; i < path.Length; i++)
             {
                 string next = path[i].ToString();
-                if ( next == seperator)
+                if (next == _seperator)
                 {
                     if (lastLetter == "")
                     {
@@ -46,14 +44,14 @@ namespace Training.LeetCode.februari.week1
                         lastLetter = next;
                     }
                 }
-                else if (next == dot)
+                else if (next == _dot)
                 {
-                    if (lastLetter == dot)
+                    if (lastLetter == _dot)
                     {
                         command += next;
                         continue;
                     }
-                    else if ( lastLetter != seperator)
+                    else if (lastLetter != _seperator)
                     {
                         command += next;
                         lastLetter = next;
@@ -68,7 +66,7 @@ namespace Training.LeetCode.februari.week1
                 }
                 else
                 {
-                    if (lastLetter == seperator)
+                    if (lastLetter == _seperator)
                     {
                         commandList.Add(command);
                         command = next;
@@ -85,12 +83,12 @@ namespace Training.LeetCode.februari.week1
             commandList.Add(command);
             for (int i = 0; i < commandList.Count; i++)
             {
-                if (commandList[i] == seperator || commandList[i] == dot)
+                if (commandList[i] == _seperator || commandList[i] == _dot)
                 {
                     commandList.RemoveAt(i);
                     i--;
                 }
-                else if (commandList[i] == back)
+                else if (commandList[i] == _back)
                 {
                     if (i > 0)
                     {
@@ -104,9 +102,7 @@ namespace Training.LeetCode.februari.week1
                     }
                 }
             }
-            string simple = seperator;
-            simple += String.Join(seperator, commandList);
-            return simple;
+            return commandList;
         }
     }
 }
