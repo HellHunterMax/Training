0c14a8f4982a416516c622cd22ba1aeb59c9d3c6
Max <hellhunter15@hotmail.com>
2021-02-06 01:34:57 +0100

    created placeholder SimplifyPath class;

---
+++ Training/LeetCode/februari/week1/SimplifyPath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week1
+{
+    public static class SimplifyPath
+    {
+        public static string Simplify(string path)
+        {
+            return FirstTry(path);
+        }
+
+        private static string FirstTry(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
