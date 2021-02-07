4fddab5867d09234e52f50318644b94ead45cab6
Max <hellhunter15@hotmail.com>
2021-02-07 03:11:16 +0100

    Finished without tests;

--- Training/LeetCode/februari/week1/BinaryTreeRightSideView.cs
+++ Training/LeetCode/februari/week1/BinaryTreeRightSideView.cs
@@ -24,7 +24,49 @@ namespace Training.LeetCode.februari.week1
 
         public static IList<int> FirstTry(TreeNode root)
         {
-            throw new NotImplementedException();
+            List<List<TreeNode>> tree = new List<List<TreeNode>>();
+
+            if (root == null)
+            {
+                return new int[0];
+            }
+
+            tree.Add(new List<TreeNode> { root });
+            for (int i = 0; i < tree.Count; i++)
+            {
+                List<TreeNode> treeNodes = GetTreeNodes(tree[i]);
+                if (treeNodes.Count == 0)
+                {
+                    break;
+                }
+                tree.Add(treeNodes);
+            }
+            int[] rightSideView = new int[tree.Count];
+            for (int i = 0; i < tree.Count; i++)
+            {
+                rightSideView[i] = tree[i][tree[i].Count - 1].val;
+            }
+            return rightSideView;
+
+        }
+
+        private static List<TreeNode> GetTreeNodes(List<TreeNode> nodes)
+        {
+            List<TreeNode> treeNodes = new List<TreeNode>();
+            foreach (TreeNode node in nodes)
+            {
+                TreeNode l = node.left;
+                TreeNode r = node.right;
+                if (l != null)
+                {
+                    treeNodes.Add(l);
+                }
+                if (r != null)
+                {
+                    treeNodes.Add(r);
+                }
+            }
+            return treeNodes;
         }
     }
 }
