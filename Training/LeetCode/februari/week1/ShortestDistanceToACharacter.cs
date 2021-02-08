165761c6c5405a7c9bb159bfcd4bc9081c5bd519
Max <hellhunter15@hotmail.com>
2021-02-08 01:16:00 +0100

    created placeholder class;

---
+++ Training/LeetCode/februari/week1/ShortestDistanceToACharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week1
+{
+    public static class ShortestDistanceToACharacter
+    {
+        public static int[] ShortestToChar(string s, char c)
+        {
+            return FirstTry(s, c);
+        }
+
+        private static int[] FirstTry(string s, char c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
