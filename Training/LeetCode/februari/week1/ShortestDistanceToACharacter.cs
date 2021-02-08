5167dd92fc43d19554808b8309c762f13e5cde7a
Max <hellhunter15@hotmail.com>
2021-02-08 01:46:09 +0100

    finished FirstTry;

--- Training/LeetCode/februari/week1/ShortestDistanceToACharacter.cs
+++ Training/LeetCode/februari/week1/ShortestDistanceToACharacter.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Training.LeetCode.februari.week1
 {
@@ -13,7 +11,38 @@ namespace Training.LeetCode.februari.week1
 
         private static int[] FirstTry(string s, char c)
         {
-            throw new NotImplementedException();
+            int[] arrF = new int[s.Length];
+            int[] arrB = new int[s.Length];
+
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            string r = new string(charArray);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                arrF[i] = s.IndexOf(c, i) - i;
+                arrB[i] = r.IndexOf(c, i) - i;
+            }
+
+            Array.Reverse(arrB);
+            int[] final = new int[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (arrF[i] < 0)
+                {
+                    final[i] = arrB[i];
+                }
+                else if (arrB[i] < 0)
+                {
+                    final[i] = arrF[i];
+                }
+                else
+                {
+                    final[i] = arrF[i] < arrB[i] ? arrF[i] : arrB[i];
+                }
+            }
+            return final;
         }
     }
 }
