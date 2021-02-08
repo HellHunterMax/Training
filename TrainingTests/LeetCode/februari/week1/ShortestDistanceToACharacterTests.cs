5054db5d3885dbb48ca9d86bc1658d3c7c8e6d98
Max <hellhunter15@hotmail.com>
2021-02-08 01:27:47 +0100

    created tests;

---
+++ TrainingTests/LeetCode/februari/week1/ShortestDistanceToACharacterTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week1;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week1
+{
+    public class ShortestDistanceToACharacterTests
+    {
+        [InlineData(new int[] { 3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0 }, "loveleetcode", 'e')]
+        [InlineData(new int[] { 3, 2, 1, 0 }, "aaab", 'b')]
+        [InlineData(new int[] { 0 }, "b", 'b')]
+        [Theory]
+        public void ShortestDistanceToACharacter_Given_StringAndChar_Returns_ArrayWithDistanceToChar(int[] expected, string s, char c)
+        {
+            var actual = ShortestDistanceToACharacter.ShortestToChar(s, c);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
