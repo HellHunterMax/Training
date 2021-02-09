d3e416b1612256caea138cfe665669c750e62403
Max <hellhunter15@hotmail.com>
2021-02-09 10:13:58 +0100

    Created Placeholder Class;

---
+++ Training/LeetCode/februari/week2/ConvertBstToGreaterTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week1;
+
+namespace Training.LeetCode.februari.week2
+{
+    public static class ConvertBstToGreaterTree
+    {
+        public static TreeNode ConvertBST(TreeNode root)
+        {
+            return FirstTry(root);
+        }
+
+        private static TreeNode FirstTry(TreeNode root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
