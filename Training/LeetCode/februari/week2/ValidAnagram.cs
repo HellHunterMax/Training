cc9e5e37cdf6c5426a30c25402c65617c3cc3cb8
Max <hellhunter15@hotmail.com>
2021-02-12 00:54:13 +0100

    created Placeholder Class;

---
+++ Training/LeetCode/februari/week2/ValidAnagram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week2
+{
+    public static class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            return FirstTry(s, t);
+        }
+
+        public static bool FirstTry(string s, string t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
