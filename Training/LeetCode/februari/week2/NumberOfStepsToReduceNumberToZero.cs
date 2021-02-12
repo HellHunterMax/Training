86f463dc61f36eac337ca529d5c171bce6be3a57
Max <hellhunter15@hotmail.com>
2021-02-13 00:21:19 +0100

    created Placeholder Class;

---
+++ Training/LeetCode/februari/week2/NumberOfStepsToReduceNumberToZero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week2
+{
+    public static class NumberOfStepsToReduceNumberToZero
+    {
+        public static int NumberOfSteps(int num)
+        {
+            return FirstTry(num);
+        }
+
+        public static int FirstTry (int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
