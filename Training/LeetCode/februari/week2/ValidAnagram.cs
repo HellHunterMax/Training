7e956d841326011d92edffa1e070169be0491577
Max <hellhunter15@hotmail.com>
2021-02-12 01:37:31 +0100

    finished SecondTry;

--- Training/LeetCode/februari/week2/ValidAnagram.cs
+++ Training/LeetCode/februari/week2/ValidAnagram.cs
@@ -9,7 +9,7 @@ namespace Training.LeetCode.februari.week2
     {
         public static bool IsAnagram(string s, string t)
         {
-            return FirstTry(s, t);
+            return SecondTry(s, t);
         }
 
         public static bool FirstTry(string s, string t)
@@ -25,7 +25,53 @@ namespace Training.LeetCode.februari.week2
         }
         public static bool SecondTry(string s, string t)
         {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            Dictionary<char, int> sDic = new Dictionary<char, int>();
+            Dictionary<char, int> tDic = new Dictionary<char, int>();
 
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (sDic.ContainsKey(s[i]))
+                {
+                    sDic[s[i]]++;
+                }
+                else
+                {
+                    sDic.Add(s[i], 1);
+                }
+
+                if (tDic.ContainsKey(t[i]))
+                {
+                    tDic[t[i]]++;
+                }
+                else
+                {
+                    tDic.Add(t[i], 1);
+                }
+                
+            }
+            if (sDic.Count != tDic.Count)
+            {
+                return false;
+            }
+            foreach(var c in sDic)
+            {
+                if (tDic.ContainsKey(c.Key))
+                {
+                    if (tDic[c.Key] != c.Value)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
