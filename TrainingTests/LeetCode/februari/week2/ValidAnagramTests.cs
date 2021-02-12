d6c73ff2cfcf14f4f2daf97e873a1b4155ace431
Max <hellhunter15@hotmail.com>
2021-02-12 01:09:28 +0100

    created tests;

---
+++ TrainingTests/LeetCode/februari/week2/ValidAnagramTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week2;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week2
+{
+    public class ValidAnagramTests
+    {
+        [InlineData(true, "s", "s")]
+        [InlineData(false, "t", "s")]
+        [InlineData(true, "anagram", "nagaram")]
+        [InlineData(false, "rat", "Car")]
+        [InlineData(false, "tts", "stt")]
+        [Theory]
+        public void IsAnagram_Given_TwoStrings_Returns_IfAnagram(bool expected, string s, string t)
+        {
+            bool actual = ValidAnagram.IsAnagram(s, t);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
