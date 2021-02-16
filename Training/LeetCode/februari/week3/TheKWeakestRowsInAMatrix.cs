1e032da468079f46901de2953ee18fa3fd372f04
Max <hellhunter15@hotmail.com>
2021-02-16 04:33:13 +0100

    created placeholder Class;

---
+++ Training/LeetCode/februari/week3/TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week3
+{
+    public static class TheKWeakestRowsInAMatrix
+    {
+        public static int[] KWeakestRows(int[][] mat, int k)
+        {
+            return FirstTry(mat, k);
+        }
+
+        private static int[] FirstTry(int[][] mat, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
