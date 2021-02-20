6d868e54f3c7389ff2a88a0afc2ba8a43673d2a1
Max <hellhunter15@hotmail.com>
2021-02-20 04:54:22 +0100

    Made Tests; Finished Problem;

--- Training/LeetCode/februari/week3/MinimumRemoveToMakeValidParentheses.cs
+++ Training/LeetCode/februari/week3/MinimumRemoveToMakeValidParentheses.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Training.LeetCode.februari.week3
 {
@@ -11,9 +9,65 @@ namespace Training.LeetCode.februari.week3
             return FirstTry(s);
         }
 
+
         public static string FirstTry(string s)
         {
-            throw new NotImplementedException();
+            s = RemoveWrongChars(s, true);
+            s = Reverse(s);
+            s= RemoveWrongChars(s, false);
+            return Reverse(s);
+        }
+
+        private static string RemoveWrongChars(string s, bool isForWard)
+        {
+            char openParetheses = '(';
+            char closeParentheses = ')';
+
+            if (!isForWard)
+            {
+                openParetheses = ')';
+                closeParentheses = '(';
+            }
+
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == openParetheses || c == closeParentheses)
+                {
+                    if (c == openParetheses)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if (RemoveChar(count))
+                        {
+                            s = s.Remove(i, 1);
+                            i--;
+                        }
+                        else
+                        {
+                            count--;
+                        }
+                    }
+                }
+            }
+            return s;
+        }
+        private static bool RemoveChar(int count)
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
---
+++ TrainingTests/LeetCode/februari/week3/MinimumRemoveToMakeValidParenthesesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Training.LeetCode.februari.week3;
+using System.Linq;
+
+namespace TrainingTests.LeetCode.februari.week3
+{
+    public class MinimumRemoveToMakeValidParenthesesTests
+    {
+        [InlineData(new string[] { "" }, ")(")]
+        [InlineData(new string[] { "" }, "))((")]
+        [InlineData(new string[] {"lee(t(co)de)", "lee(t(c)ode)", "lee(t(c)o)de" }, "lee(t(c)o)de)")]
+        [InlineData(new string[] { "ab(c)d" }, "a)b(c)d")]
+        [InlineData(new string[] { "a(b(c)d)", "(ab(c)d)", "(a(bc)d)" }, "(a(b(c)d)")]
+
+        [Theory]
+        public void MinimumRemoveToMakeValidParentheses_Given_String_Returns_StringWithValidParentheses(string[] expected, string s)
+        {
+            var actual = MinimumRemoveToMakeValidParentheses.MinRemoveToMakeValid(s);
+
+            Assert.Contains(actual, expected);
+        }
+    }
+}
