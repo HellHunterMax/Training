0d7d842aab1c46e57f72efde6ce91436d65cd515
Max <hellhunter15@hotmail.com>
2021-02-20 04:19:51 +0100

    created placeholder class;

---
+++ Training/LeetCode/februari/week3/MinimumRemoveToMakeValidParentheses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week3
+{
+    public static class MinimumRemoveToMakeValidParentheses
+    {
+        public static string MinRemoveToMakeValid(string s)
+        {
+            return FirstTry(s);
+        }
+
+        public static string FirstTry(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
