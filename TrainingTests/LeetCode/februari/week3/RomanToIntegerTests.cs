cbcf6a5d0e4fc43cbaff16f56e1073aa7e5888d8
Max <hellhunter15@hotmail.com>
2021-02-21 03:18:41 +0100

    created tests;

---
+++ TrainingTests/LeetCode/februari/week3/RomanToIntegerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week3;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week3
+{
+    public class RomanToIntegerTests
+    {
+        [InlineData(1, "I")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(50, "L")]
+        [InlineData(100, "C")]
+        [InlineData(500, "D")]
+        [InlineData(58, "LVIII")]
+        [InlineData(1994, "MCMXCIV")]
+        [InlineData(1000, "M")]
+        [Theory]
+        public void RomanToInteger_Given_RomanLetters_Returns_Integer(int expected, string roman)
+        {
+            var actual = RomanToInteger.RomanToInt(roman);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
