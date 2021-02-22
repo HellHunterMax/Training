c6d3a8ac4b79ed6a3f34412579566620ba4be912
Max <hellhunter15@hotmail.com>
2021-02-22 01:05:15 +0100

    created placeholder Class;

---
+++ Training/LeetCode/februari/week3/BrokenCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week3
+{
+    public static class BrokenCalculator
+    {
+        public static int calculate(int X, int Y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
