24461caf6d2b8cfd62e356d4a6607cfeb0df04aa
Max <hellhunter15@hotmail.com>
2021-02-23 03:32:52 +0100

    created placeholder class;

---
+++ Training/LeetCode/februari/week4/LongestWordInDictionaryThroughDeleting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week4
+{
+    public static class LongestWordInDictionaryThroughDeleting
+    {
+        public static string FindLongestWord(string s, IList<string> d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
