dfde7e2960493f009ae9eb91c15fcab4ac9ea22a
Max <hellhunter15@hotmail.com>
2021-02-24 00:16:52 +0100

    created placeholder Class;

---
+++ Training/LeetCode/februari/week4/SearchA2DMatrixII.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week4
+{
+    public static class SearchA2DMatrixII
+    {
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
