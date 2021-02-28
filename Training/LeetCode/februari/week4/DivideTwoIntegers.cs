c0c189fdc876abd5823a0238c0df5ee95e831e8b
Max <hellhunter15@hotmail.com>
2021-02-28 02:39:37 +0100

    first ittearion without minus;

--- Training/LeetCode/februari/week4/DivideTwoIntegers.cs
+++ Training/LeetCode/februari/week4/DivideTwoIntegers.cs
@@ -10,5 +10,64 @@ namespace Training.LeetCode.februari.week4
         {
             throw new NotImplementedException();
         }
+
+        private static int FirstTry(int dividend, int divisor)
+        {
+            if (divisor == 1)
+            {
+                return dividend;
+            }
+            if (divisor == -1)
+            {
+                return 0 - dividend;
+            }
+
+            int answer = 0;
+            int absDividend = Math.Abs(dividend);
+            int absDivisor = Math.Abs(divisor);
+
+            int half = FindHalf(absDivisor);
+
+            int remaining = absDividend;
+
+            for (int i = 0; i < absDividend; i++)
+            {
+                if (remaining > absDivisor)
+                {
+                    if (remaining >= half)
+                    {
+                        answer++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    answer++;
+                    remaining -= absDivisor;
+                }
+            }
+
+            return answer;
+        }
+
+        private static int FindHalf(int absDivisor)
+        {
+            int count = 0;
+            int half = 0;
+
+            for (int i = 0; i < absDivisor; i++)
+            {
+                half++;
+                count += 2;
+                if (count >= absDivisor)
+                {
+                    break;
+                }
+            }
+            return half;
+        }
     }
 }
