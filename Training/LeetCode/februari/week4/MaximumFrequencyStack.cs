4308392c3d1eaf9c04bb1071d3462da4228ed32a
Max <hellhunter15@hotmail.com>
2021-03-01 00:30:30 +0100

    FirstTry;

--- Training/LeetCode/februari/week4/MaximumFrequencyStack.cs
+++ Training/LeetCode/februari/week4/MaximumFrequencyStack.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Training.LeetCode.februari.week4
@@ -10,23 +11,80 @@ namespace Training.LeetCode.februari.week4
 
     public class FreqStack
     {
-        private List<int> _list;
-        Dictionary<int, int> _dic;
+        private List<int> _numbers;
+        private Dictionary<int, int> _dic;
 
         public FreqStack()
         {
-            _list = new List<int>();
+            _numbers = new List<int>();
             _dic = new Dictionary<int, int>();
         }
 
         public void Push(int x)
         {
-            throw new NotImplementedException();
+            _numbers.Add(x);
+            if(_dic.ContainsKey(x))
+            {
+                _dic[x]++;
+            }
+            else
+            {
+                _dic.Add(x, 1);
+            }
         }
 
         public int Pop()
         {
-            throw new NotImplementedException();
+            Sort_dic();
+            int lastIndex = -1;
+            int chosenNumber = -1;
+            var numbers = FindMostNumbers();
+
+            foreach (var number in numbers)
+            {
+                int index = _numbers.LastIndexOf(number);
+
+                if(lastIndex < index)
+                {
+                    lastIndex = index;
+                    chosenNumber = number;
+                }
+            }
+
+            _dic[chosenNumber]--;
+            if (_dic[chosenNumber] == 0)
+            {
+                _dic.Remove(chosenNumber);
+            }
+            _numbers.RemoveAt(lastIndex);
+
+            return chosenNumber;
+        }
+
+        private List<int> FindMostNumbers()
+        {
+            List<int> numbers = new List<int>();
+            int freq = _dic.First().Value;
+
+            foreach (var pair in _dic)
+            {
+                if (pair.Value == freq)
+                {
+                    numbers.Add(pair.Key);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return numbers;
+        }
+
+        private void Sort_dic()
+        {
+            _dic = (Dictionary<int, int>)(from pair in _dic
+                   orderby pair.Value descending
+                   select pair).ToDictionary(pair => pair.Key, pair => pair.Value); ;
         }
     }
 }
