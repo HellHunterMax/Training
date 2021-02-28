91e29284bd8f4b7e0844bf59d3eb624349589a8f
Max <hellhunter15@hotmail.com>
2021-02-28 02:20:41 +0100

    created tests;

---
+++ TrainingTests/LeetCode/februari/week4/DivideTwoIntegersTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week4;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week4
+{
+    public class DivideTwoIntegersTests
+    {
+        [InlineData(1, 1, 1)]
+        [InlineData(0, 0, 1)]
+        [InlineData(2, 4, 2)]
+        [InlineData(0, 1, 3)]
+        [InlineData(3, 10, 3)]
+        [InlineData(-2, 7, -3)]
+        [InlineData(1, 1, 1)]
+        [Theory]
+        public void DivideTwoIntegers_Given_TwoInts_Divides(int expected, int dividend, int divisor)
+        {
+            var actual = DivideTwoIntegers.Divide(dividend, divisor);
+
+            Assert.Equal(expected, actual);
+
+
+        }
+    }
+}
