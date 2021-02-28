5474a45fded9ab22324d1f64f5ddaccd8fa0d753
Max <hellhunter15@hotmail.com>
2021-02-28 02:14:26 +0100

    created placeholder class;

---
+++ Training/LeetCode/februari/week4/DivideTwoIntegers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week4
+{
+    public static class DivideTwoIntegers
+    {
+        public static int Divide(int dividend, int divisor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
