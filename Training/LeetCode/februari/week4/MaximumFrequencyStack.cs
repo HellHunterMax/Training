d48302ab6a4d1e4d87efe7b09fed361f5e653fa9
Max <hellhunter15@hotmail.com>
2021-02-28 23:44:00 +0100

    created placeholder class;

---
+++ Training/LeetCode/februari/week4/MaximumFrequencyStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week4
+{
+    public static class MaximumFrequencyStack
+    {
+    }
+
+    public class FreqStack
+    {
+        private List<int> _list;
+        Dictionary<int, int> _dic;
+
+        public FreqStack()
+        {
+            _list = new List<int>();
+            _dic = new Dictionary<int, int>();
+        }
+
+        public void Push(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
