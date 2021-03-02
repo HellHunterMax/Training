958754ab47da1289076b1bc60e332b8795789a69
Max <hellhunter15@hotmail.com>
2021-03-02 01:14:23 +0100

    finished with speedup;

--- Training/LeetCode/februari/week1/DistributeCandies.cs
+++ Training/LeetCode/februari/week1/DistributeCandies.cs
@@ -6,9 +6,32 @@ namespace Training.LeetCode.februari.week1
 {
     public static class DistributeCandies
     {
+        //https://leetcode.com/problems/distribute-candies/submissions/
         public static int Distribute(int[] candyType)
         {
-            throw new NotImplementedException();
+            int numberOfCandiesToBeEaten = candiesDevidedBy2(candyType);
+            HashSet<int> hash = new HashSet<int>();
+
+            for (int i = 0; i < candyType.Length; i++)
+            {
+                
+                if(!hash.Contains(candyType[i]))
+                {
+                    hash.Add(candyType[i]);
+                    if (hash.Count == numberOfCandiesToBeEaten)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return hash.Count;
+
+        }
+
+        private static int candiesDevidedBy2(int[] candyType)
+        {
+            return candyType.Length / 2;
         }
     }
 }
