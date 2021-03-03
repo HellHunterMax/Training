7de56accf90c7d3170a4287d68d544784d980095
Max <hellhunter15@hotmail.com>
2021-03-03 02:24:22 +0100

    Explanation wasnt good; redoing with new info; numbers do not have to be in order;

--- Training/LeetCode/februari/week1/SetMismatch.cs
+++ Training/LeetCode/februari/week1/SetMismatch.cs
@@ -8,7 +8,34 @@ namespace Training.LeetCode.februari.week1
     {
         public static int[] FindErrorNums(int[] nums)
         {
-            throw new NotImplementedException();
+            bool forward = nums[0] == 1 ? true: false;
+
+            return forward ? Forward(nums) : Backward(nums);
+        }
+        
+        private static int[] Forward(int[] nums)
+        {
+            for (int i = 0; i < nums.Length +1; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return new int[] { nums[i], i + 1 };
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+        private static int[] Backward(int[] nums)
+        {
+            int number = 1;
+            for (int i = nums.Length -1; i >= 0; i--, number++)
+            {
+                if (nums[i] != number)
+                {
+                    return new int[] { nums[i], number };
+                }
+            }
+
+            return new int[] { -1, -1 };
         }
     }
 }
