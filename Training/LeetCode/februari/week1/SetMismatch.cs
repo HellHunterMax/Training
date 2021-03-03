71bbe9dd40a24d20e7f39154e0f71a8977349ed8
Max <hellhunter15@hotmail.com>
2021-03-03 03:03:31 +0100

    works as intended now;

--- Training/LeetCode/februari/week1/SetMismatch.cs
+++ Training/LeetCode/februari/week1/SetMismatch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Training.LeetCode.februari.week1
@@ -8,23 +9,87 @@ namespace Training.LeetCode.februari.week1
     {
         public static int[] FindErrorNums(int[] nums)
         {
+            int duplicate = -1;
+            int missingNumber = -1;
+
             Array.Sort(nums);
 
-            return FindMissingNumber(nums);
+            var duplicateAndIndex = FindDuplicate(nums);
+            duplicate = duplicateAndIndex[0];
+
+            var numsList = nums.ToList();
+            numsList.RemoveAt(duplicateAndIndex[1]);
+
+            missingNumber = FindMissingNumber(numsList);
+
+            return new int[] { duplicate, missingNumber };
+        }
+
+        private static int FindMissingNumber(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            { 
+                if (nums[i] != i +1)
+                {
+                    return i + 1;
+                }
+            }
+            return nums.Count +1;
         }
         
-        private static int[] FindMissingNumber(int[] nums)
+        /// <summary>
+        /// finds dunplicate number and returns the number and index
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns>First number, Second index</returns>
+        private static int[] FindDuplicate(int[] nums)
         {
-            for (int i = 0; i < nums.Length +1; i++)
+            HashSet<int> hash = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] != i + 1)
+                if (!hash.Contains(nums[i]))
                 {
-                    return new int[] { nums[i], i + 1 };
+                    hash.Add(nums[i]);
+                }
+                else
+                {
+                    return new int[] { nums[i], i };
                 }
             }
             return new int[] { -1, -1 };
         }
         /*
+         * 
+        private static int[] FindMissingNumber(int[] nums)
+        {
+            int missingNumber = -1;
+            int duplicate = -1;
+            if (nums[0] != 1)
+            {
+                missingNumber = 1;
+            }
+            HashSet<int> hash = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (missingNumber > 0 && duplicate > 0)
+                {
+                    break;
+                }
+                if (!hash.Contains(nums[i]))
+                {
+                    hash.Add(nums[i]);
+                }
+                else
+                {
+                    duplicate = nums[i];
+                }
+                if (missingNumber < 0 && nums[i] != i +1)
+                {
+                    missingNumber = i + 1;
+                }
+            }
+            return new int[] { duplicate, missingNumber };
+        }
         private static int[] Backward(int[] nums)
         {
             int number = 1;
--- TrainingTests/LeetCode/februari/week1/SetMismatchTests.cs
+++ TrainingTests/LeetCode/februari/week1/SetMismatchTests.cs
@@ -12,6 +12,8 @@ namespace TrainingTests.LeetCode.februari.week1
         [InlineData(new int[] { 2, 3 }, new int[] { 1, 2, 2, 4 })]
         [InlineData(new int[] { 2, 1 }, new int[] { 3, 2, 2 })]
         [InlineData(new int[] { 3, 2 }, new int[] { 8, 7, 3, 5, 3, 6, 1, 4 })]
+        [InlineData(new int[] { 3, 1 }, new int[] { 3, 2, 3, 4, 6, 5 })]
+        [InlineData(new int[] { 2, 10 }, new int[] { 1, 5, 3, 2, 2, 7, 6, 4, 8, 9 })]
         [Theory]
         public void FindErrorNums_Given_Array_Returns_TwoNumbersFirstOriginalSecondExpected(int[] expected, int[] s)
         {
