f6aaa9d54e47757924a924e3fa7525afa3be7076
Max <hellhunter15@hotmail.com>
2021-03-03 02:07:02 +0100

    created Tests; created Placeholder class;

---
+++ Training/LeetCode/februari/week1/SetMismatch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week1
+{
+    public static class SetMismatch
+    {
+        public static int[] FindErrorNums(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrainingTests/LeetCode/februari/week1/SetMismatchTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week1;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week1
+{
+    public class SetMismatchTests
+    {
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 1})]
+        [InlineData(new int[] { 2, 3 }, new int[] { 1, 2, 2, 4 })]
+        [Theory]
+        public void FindErrorNums_Given_Array_Returns_TwoNumbersFirstOriginalSecondExpected(int[] expected, int[] s)
+        {
+            var actual = SetMismatch.FindErrorNums(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
