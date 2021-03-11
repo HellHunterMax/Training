57174da676f38dcf401f08847211dac9046a243b
Max <hellhunter15@hotmail.com>
2021-03-11 03:52:36 +0100

    added class for IntegerToRoman with placeholder method IntToRoman and Convert Method

---
+++ Training/LeetCode/februari/week2/IntegerToRoman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.LeetCode.februari.week2
+{
+    public static class IntegerToRoman
+    {
+        private static string IntToRoman(int num)
+        {
+            throw new NotImplementedException();
+        }
+        public static string Convert(int num)
+        {
+            return IntToRoman(num);
+        }
+    }
+}
