7f173a52eb0fa0f95585ed1fb76fb0a0c9532e85
Max <hellhunter15@hotmail.com>
2021-03-11 04:04:05 +0100

    Created UnitTests for Convert Method in IntergetToRoman

---
+++ TrainingTests/LeetCode/februari/week2/IntegerToRomanTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.LeetCode.februari.week2;
+using Xunit;
+
+namespace TrainingTests.LeetCode.februari.week2
+{
+    public class IntegerToRomanTests
+    {
+        [InlineData("I", 1)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
+        [InlineData("VI", 6)]
+        [InlineData("VII", 7)]
+        [InlineData("VIII", 8)]
+        [InlineData("IX", 9)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
+        [InlineData("MMMCMXCIX", 100)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [Theory]
+        public void Convert_Given_Int_Returns_RomanString(string expected, int num)
+        {
+            string actual = IntegerToRoman.Convert(num);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
