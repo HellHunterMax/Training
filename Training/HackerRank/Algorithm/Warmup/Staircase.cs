e0853154a3d70b68a96441bda9682fc419f1e5b8
Max <hellhunter15@hotmail.com>
2021-11-28 06:17:40 +0100

    Add: Hackerrank Staircase

---
+++ Training/HackerRank/Algorithm/Warmup/Staircase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.HackerRank.Algorithm.Warmup
+{
+    public class Staircase
+    {
+        public static void staircase(int n)
+        {
+            for (int i = 1; i < n +1; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(' ', n -i);
+                sb.Append('#', i);
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
