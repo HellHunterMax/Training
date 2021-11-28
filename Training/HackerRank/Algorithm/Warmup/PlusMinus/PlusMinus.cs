e939a5267c9cdcd307f8154bcbac24630b4dc939
Max <hellhunter15@hotmail.com>
2021-11-28 06:07:42 +0100

    Add: HackerRank Plusminus

---
+++ Training/HackerRank/Algorithm/Warmup/PlusMinus/PlusMinus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.HackerRank.Algorithm.Warmup.PlusMinus
+{
+    public static class PlusMinus
+    {
+        static void plusMinus(int[] arr)
+        {
+            var total = arr.Length;
+            decimal plus = 0;
+            decimal negative = 0;
+            decimal zero = 0;
+
+            for (var i = 0; i < total; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    negative++;
+                    continue;
+                }
+                if (arr[i] > 0)
+                {
+                    plus++;
+                    continue;
+                }
+                zero++;
+                /*
+                switch (arr[i])
+                {
+                    case < 0:
+                        negative++;
+                        break;
+                    case > 0:
+                        plus++;
+                        break;
+                    default:
+                        zero++;
+                        break;
+                }
+                */
+            }
+            var plusDecimal = plus / total;
+            decimal negativeDecimal = negative / total;
+            decimal zeroDecimal = zero / total;
+
+            Console.WriteLine(plusDecimal);
+            Console.WriteLine(negativeDecimal);
+            Console.WriteLine(zeroDecimal);
+        }
+    }
+}
--- Training/Program.cs
+++ Training/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Training
 {
@@ -6,6 +8,51 @@ namespace Training
     {
         static void Main(string[] args)
         {
+            Person person = new Person() { Id = "1", UserName = "Max"};
+            person.Jokes.Add(1);
+            var list  = person.Jokes.ToList();
+            person.Jokes.Add(2);
+            Console.WriteLine("This is a List");
+            Console.WriteLine(list[0]);
+            foreach (var joke in list)
+            {
+                Console.WriteLine(joke);
+            }
+            Console.WriteLine("End of list");
+            Console.WriteLine("This is a ICollection");
+            Console.WriteLine(person.Jokes.ElementAt(0));
+            foreach (var joke in person.Jokes)
+            {
+                Console.WriteLine(joke);
+            }
+            Console.WriteLine("End of ICollection");
+            person.Jokes = new int[2];
+            Console.WriteLine(person.Jokes);
+
         }
+
+        public static void GuessNunber()
+        {
+            int number = 0;
+            var random = new Random();
+
+            int guess = random.Next(0, 3);
+            Console.WriteLine(guess);
+            if (number == 10)
+            {
+                return;
+            }
+            number++;
+            Console.ReadKey();
+            GuessNunber();
+        }
+        public class Person
+        {
+            public string Id { get; set; }
+            public string UserName { get; set; }
+            public ICollection<int> Jokes { get; set; } = new List<int>();
+
+        }
+
     }
 }
