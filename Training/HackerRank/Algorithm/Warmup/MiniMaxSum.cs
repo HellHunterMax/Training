f4a7eacd408839a699767cd36d77851e26f5b6d0
Max <hellhunter15@hotmail.com>
2021-11-28 06:27:34 +0100

    Add: HackerRank MiniMaxSum

---
+++ Training/HackerRank/Algorithm/Warmup/MiniMaxSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.HackerRank.Algorithm.Warmup
+{
+    public static class MiniMaxSum
+    {
+        public static void miniMaxSum(List<int> arr)
+        {
+            int length = arr.Count;
+
+            arr.Sort();
+
+            long smallest = AddFour(arr[0], arr[1], arr[2], arr[3]);
+            long largest = AddFour(arr[length - 1], arr[length - 2], arr[length - 3], arr[length - 4]);
+            Console.WriteLine($"{smallest} {largest}");
+
+
+        }
+
+        private static long AddFour(int a, int b, int c, int d)
+        {
+            return (long) a + b + c + d;
+        }
+    }
+}
